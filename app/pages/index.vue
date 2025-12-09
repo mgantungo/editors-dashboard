12245de4bae14c295b5fe6805f5a7f6c18db25bd
mgantungo <mgantungo@gmail.com>
2025-12-09 22:05:32 +0300

    update to ad index

---
+++ app/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+
+</template>
+
+<script setup>
+
+const router = useRouter()
+
+onMounted(() => {
+  router.replace('/login')
+})
+
+// OR even simpler with navigateTo
+//navigateTo('/login')
+</script>
